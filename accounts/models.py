c10a37ee753a8ee1ee618d6e0c9373dbc82c8679
= <=>
2023-01-13 20:08:48 +0200

    Update required field

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser, UserManager
+
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -15,7 +16,7 @@ class User(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
     phone_number = PhoneNumberField(unique=True, null=False, blank=False)
 
-    REQUIRED_FIELDS = ['username', ]
+    # REQUIRED_FIELDS = ['username', ]
     USERNAME_FIELD = 'username'
 
     objects = CustomUserManager()
